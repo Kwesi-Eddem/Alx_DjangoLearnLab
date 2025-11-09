9b9752092252d2461aeef24d63251c3a36d20fd5
addem <kwesi.eddem@gmail.com>
2025-11-09 20:54:42 +0000

    7th cmmt

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 # Create your views here.
-from .models import *
+from .models import Library
+from .models import Book
 
 def list_books(request):
     books = Book.objects.all()
