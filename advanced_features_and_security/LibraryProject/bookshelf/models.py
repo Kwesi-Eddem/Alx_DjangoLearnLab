c4dd2c306b94c812c5c10135fa7a23513275bcfe
addem <kwesi.eddem@gmail.com>
2025-11-16 22:30:02 +0000

    2nd edit

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -7,32 +7,17 @@ class Book(models.Model):
     publication_year = models.IntegerField()
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, username, email=None, password=None, date_of_birth=None, profile_photo=None, **extra_fields):
-        if not username:
-            raise ValueError("The username field is required")
-        email = self.normalize_email(email)
-        user = self.model(
-            username= username,
-            email=email,
-            date_of_birth=date_of_birth,
-            profile_photo=profile_photo,
-            **extra_fields
-            )
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        user = self.model(username=username, email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
-    
-    def create_superuser(self, username, email=None, password=None,date_of_birth=None, profile_photo=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser",True)
 
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-        
-        return self.create_user(username, email, password, date_of_birth, profile_photo, **extra_fields)
-    
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(username, email, password, **extra_fields)
+
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to="profile_photos/", null=True, blank=True)
