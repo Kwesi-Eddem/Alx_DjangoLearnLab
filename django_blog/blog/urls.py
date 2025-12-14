4b4dc63f18768394a0b1992c066f627ff8aa0376
addem <kwesi.eddem@gmail.com>
2025-12-14 18:34:11 +0000

    5th cmmit

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
 from .views import UserLoginView, UserLogoutView, register, profile
-from .views import (PostListView, PostDetailView, PostCreateView, PostDeleteView, PostUpdateView)
+from .views import (
+    PostListView, PostDetailView,
+    PostCreateView, PostDeleteView, PostUpdateView
+)
+
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
     path('register/', register, name='register'),
     path('profile/', profile, name='profile'),
-    path('posts/', PostListView.as_view(), name='posts'),
-    path('posts/new/', PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete')
+
+    # Blog post CRUD routes
+    path('posts/', PostListView.as_view(), name='posts'),                 # list view
+    path('post/new/', PostCreateView.as_view(), name='post-create'),      # create view
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'), # detail view
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),  # update view
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),  # delete view
 ]
